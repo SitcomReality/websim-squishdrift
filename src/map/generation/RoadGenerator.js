f228ae794f837210d5aa4442fc81dd0271606e42
SitcomReality <SitcomReality@websim.ai>
2025-08-19 15:15:38 +0000

    v474: Around the outside edge of the map, our ring road has one undefined or improperly defined lane. The most external lane is correct (the lane road tiles that are closest to the edge of the map), but the lane next to it (between the outermost lane and t

--- src/map/generation/RoadGenerator.js
+++ src/map/generation/RoadGenerator.js
@@ -13,29 +13,45 @@ export class RoadGenerator {
     
     // Generate roundabouts at intersections
     this.generateRoundabouts(tiles);
-
-    // Add zebra crossings to inner perimeter lanes between intersections
-    this.addPerimeterInnerCrossings(tiles);
   }
 
   generatePerimeterRoad(tiles) {
-    const width = this.cityLayout.width;
-    const height = this.cityLayout.height;
+    const { width, height, blocksWide, blocksHigh } = this.cityLayout;
+
+    const set = (x, y, t) => {
+      if (x >= 0 && y >= 0 && x < width && y < height) {
+        tiles[y][x] = t;
+      }
+    };
+
+    const gaps = [];
+    for (let gy = 0; gy <= blocksHigh; gy++) {
+      for (let gx = 0; gx <= blocksWide; gx++) {
+        const center = this.cityLayout.getIntersectionCenter(gx, gy);
+        gaps.push({ cx: center.x, cy: center.y });
+      }
+    }
 
     // Top and bottom lanes
-    for (let i = 0; i < width; i++) {
-      tiles[0][i] = Tile.RoadW;
-      tiles[1][i] = Tile.RoadW;
-      tiles[height - 2][i] = Tile.RoadE;
-      tiles[height - 1][i] = Tile.RoadE;
+    for (let i = 1; i < width - 1; i++) {
+      if (gaps.some(g => i >= g.cx - 2 && i <= g.cx + 2)) continue;
+      
+      set(i, 1, Tile.RoadW);
+      set(i, 2, Tile.RoadW);
+
+      set(i, height - 3, Tile.RoadE);
+      set(i, height - 2, Tile.RoadE);
     }
 
     // Left and right lanes
-    for (let i = 0; i < height; i++) {
-      tiles[i][0] = Tile.RoadS;
-      tiles[i][1] = Tile.RoadS;
-      tiles[i][width - 2] = Tile.RoadN;
-      tiles[i][width - 1] = Tile.RoadN;
+    for (let i = 1; i < height - 1; i++) {
+      if (gaps.some(g => i >= g.cy - 2 && i <= g.cy + 2)) continue;
+
+      set(1, i, Tile.RoadS);
+      set(2, i, Tile.RoadS);
+
+      set(width - 3, i, Tile.RoadN);
+      set(width - 2, i, Tile.RoadN);
     }
   }
 
@@ -150,64 +166,6 @@ export class RoadGenerator {
     for (let y = cy - 2; y <= cy + 2; y++) {
       set(isLeft ? cx - 2 : cx + 1, y, direction);
       set(isLeft ? cx - 1 : cx + 2, y, direction);
-    }
-  }
-
-  addPerimeterInnerCrossings(tiles) {
-    const W = this.cityLayout.width, H = this.cityLayout.height;
-
-    // Top inner lane: y = 1, road flows West => use ZebraCrossingW (left-right neighbors are road)
-    for (let gx = 0; gx <= this.cityLayout.blocksWide; gx++) {
-      const cx = this.cityLayout.getIntersectionCenter(gx, 0).x;
-      if (cx - 2 >= 0) {
-        tiles[1][cx - 2] = Tile.ZebraCrossingW;
-        tiles[1][cx - 1] = Tile.ZebraCrossingW;
-      }
-      if (cx + 2 < W) {
-        tiles[1][cx + 1] = Tile.ZebraCrossingW;
-        tiles[1][cx + 2] = Tile.ZebraCrossingW;
-      }
-    }
-
-    // Bottom inner lane: y = H - 2, road flows East => use ZebraCrossingE
-    for (let gx = 0; gx <= this.cityLayout.blocksWide; gx++) {
-      const cx = this.cityLayout.getIntersectionCenter(gx, this.cityLayout.blocksHigh).x;
-      const y = H - 2;
-      if (cx - 2 >= 0) {
-        tiles[y][cx - 2] = Tile.ZebraCrossingE;
-        tiles[y][cx - 1] = Tile.ZebraCrossingE;
-      }
-      if (cx + 2 < W) {
-        tiles[y][cx + 1] = Tile.ZebraCrossingE;
-        tiles[y][cx + 2] = Tile.ZebraCrossingE;
-      }
-    }
-
-    // Left inner lane: x = 1, road flows South => use ZebraCrossingS (up/down neighbors are road)
-    for (let gy = 0; gy <= this.cityLayout.blocksHigh; gy++) {
-      const cy = this.cityLayout.getIntersectionCenter(0, gy).y;
-      if (cy - 2 >= 0) {
-        tiles[cy - 2][1] = Tile.ZebraCrossingS;
-        tiles[cy - 1][1] = Tile.ZebraCrossingS;
-      }
-      if (cy + 2 < H) {
-        tiles[cy + 1][1] = Tile.ZebraCrossingS;
-        tiles[cy + 2][1] = Tile.ZebraCrossingS;
-      }
-    }
-
-    // Right inner lane: x = W - 2, road flows North => use ZebraCrossingN
-    for (let gy = 0; gy <= this.cityLayout.blocksHigh; gy++) {
-      const cy = this.cityLayout.getIntersectionCenter(this.cityLayout.blocksWide, gy).y;
-      const x = W - 2;
-      if (cy - 2 >= 0) {
-        tiles[cy - 2][x] = Tile.ZebraCrossingN;
-        tiles[cy - 1][x] = Tile.ZebraCrossingN;
-      }
-      if (cy + 2 < H) {
-        tiles[cy + 1][x] = Tile.ZebraCrossingN;
-        tiles[cy + 2][x] = Tile.ZebraCrossingN;
-      }
     }
   }
 

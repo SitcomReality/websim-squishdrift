2d5656f29115bcfe5cdc8d79d6e8bbc7c4d66723
SitcomReality <SitcomReality@websim.ai>
2025-08-29 07:55:08 +0000

    v880: In title-screen.js, we list 6 controls. The first four are shown two-per-line, but the last two are on separate lines. Move Pause up so that it's below "Space: Handbrake" and on the same line as "Shift: Sprint"

--- title-screen.js
+++ title-screen.js
@@ -3,11 +3,11 @@
               <p><strong>Mouse:</strong> Aim & Click to Shoot</p>
               <p><strong>WASD / Arrows:</strong> Move</p>
               <p><strong>Shift:</strong> Sprint</p>
-              <p><strong>P / Esc:</strong> Pause</p>
+              <p><strong>Space:</strong> Handbrake</p>
             </div>
             <div>
               <p><strong>E:</strong> Enter/Exit Vehicle</p>
-              <p><strong>Space:</strong> Handbrake</p>
+              <p><strong>P / Esc:</strong> Pause</p>
             </div>
           </div>
 

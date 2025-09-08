800d580663b50a2cc079be7ed6fbc0745d409c7d
SitcomReality <SitcomReality@websim.ai>
2025-09-08 11:52:22 +0000

    v953: We've been refactoring src/app/core/systems/PlayerSystem.js and we're currently at the stage where we have this error: Uncaught SyntaxError: Unexpected end of input (at PlayerSystem.js:371:2) Have we left some vestigial backticks or something that's

--- src/app/core/systems/PlayerSystem.js
+++ src/app/core/systems/PlayerSystem.js
@@ -1,3 +1,5 @@
+
+```javascript
 import { Vec2 } from '../../../utils/Vec2.js';
 import { Health } from '../../components/Health.js';
 import { StaminaSystem } from '../systems/player/StaminaSystem.js';

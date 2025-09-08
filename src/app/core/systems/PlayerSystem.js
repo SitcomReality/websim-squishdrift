6e493bb5c73808c361c472dbb0507c4d2753fca6
SitcomReality <SitcomReality@websim.ai>
2025-09-08 13:08:46 +0000

    v958: The imports in PlayerSystem.js are looking for files in the location. Remove the '/core' from the URL of the imports and they should be correct.

--- src/app/core/systems/PlayerSystem.js
+++ src/app/core/systems/PlayerSystem.js
@@ -1,10 +1,8 @@
 import { Vec2 } from '../../../utils/Vec2.js';
 import { Health } from '../../components/Health.js';
-import { StaminaSystem } from '../systems/player/StaminaSystem.js';
-import { HealthSystem } from '../systems/player/HealthSystem.js';
-import { FlattenSystem } from '../systems/player/FlattenSystem.js';
-import { MovementSystem } from '../systems/player/MovementSystem.js';
-import { FacingSystem } from '../systems/player/FacingSystem.js';
-import { InteractionSystem } from '../systems/player/InteractionSystem.js';
-
-
+import { StaminaSystem } from '../../systems/player/StaminaSystem.js';
+import { HealthSystem } from '../../systems/player/HealthSystem.js';
+import { FlattenSystem } from '../../systems/player/FlattenSystem.js';
+import { MovementSystem } from '../../systems/player/MovementSystem.js';
+import { FacingSystem } from '../../systems/player/FacingSystem.js';
+import { InteractionSystem } from '../../systems/player/InteractionSystem.js';
